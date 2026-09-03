cf3e26431f565e1139d0371345f6380b840b1104
Jolene Langlinais <36460856+jolanglinais@users.noreply.github.com>
2026-09-03 16:15:35 -0500

    chore(css): consolidate duplication

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
+const button = document.getElementById("lightOrDarkButton");
+
 const switchLightOrDark = () => {
-    let theme = document.getElementById("theme");
-    let el = document.getElementById("lightOrDarkButton");
-    let sigImg = document.getElementById("sig");
-    if (theme.getAttribute("href") == "styles/main.light.css") {
-        theme.href = "styles/main.dark.css";
-        el.firstChild.data = "☀️";
-        sigImg.src = "assets/signatureLight.webp";
-        sigImg.style.width = "400px";
-        sigImg.style.maxWidth = "70%";
-    } else {
-        theme.href = "styles/main.light.css";
-        el.firstChild.data = "🌙";
+    const root = document.documentElement;
+    const sigImg = document.getElementById("sig");
+
+    if (root.dataset.theme === "dark") {
+        delete root.dataset.theme;
+        button.textContent = "🌙";
         sigImg.src = "assets/signatureDark.webp";
-        sigImg.style.width = "400px";
-        sigImg.style.maxWidth = "70%";
-    };
+    } else {
+        root.dataset.theme = "dark";
+        button.textContent = "☀️";
+        sigImg.src = "assets/signatureLight.webp";
+    }
 };
 
-document.getElementById("lightOrDarkButton").addEventListener("click", switchLightOrDark);
+button.addEventListener("click", switchLightOrDark);
